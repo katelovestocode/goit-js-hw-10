0b9723041b08f01b9527c8d56faba1becebd0ebf
Kate Vaskevich <kate.panasiuk@yahoo.com>
2022-08-19 12:07:18 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -5,7 +5,10 @@ const SEARCH_FILTER = '?fields=name,capital,population,flags,languages';
 
 // go to the server, parse result and return Promise as the result method json
 export function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}${SEARCH_FILTER}`).then(response =>
-    response.json()
-  );
+  return fetch(`${BASE_URL}/name/${name}${SEARCH_FILTER}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
